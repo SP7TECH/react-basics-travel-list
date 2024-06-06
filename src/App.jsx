0bef737a28b8cd01594c418e7b5337158f499a1a
Sumit Pandey <spandey247789@gmail.com>
2024-06-07 02:00:12 +0530

    showing stats - derived state

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         onRemoveItem={handleRemoveItem}
         onUpdateItem={handleUpdateItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -110,10 +110,26 @@ function Item({ item, onRemoveItem, onUpdateItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  const numberOfItems = items.length;
+  const numberOfPackedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numberOfPackedItems / numberOfItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X %)</em>
+      <em>
+        {percentage === 100
+          ? `You got everything. Ready to go! ✈️`
+          : `💼 You have ${numberOfItems} items on your list, and you already packed ${numberOfPackedItems} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
